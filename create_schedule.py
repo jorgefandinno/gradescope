4734b1ff9c06a6b3e65de8ddec8461acd2d3efc5
jorgefandinno <jfandinno@unomaha.edu>
2023-12-20 15:29:21 -0600

    update

--- create_schedule.py
+++ create_schedule.py
@@ -1,10 +1,18 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="Updates the schedule to a new semester")
+parser.add_argument("old_schedule", type = argparse.FileType('r'), help="Path to the old schedule")
+args = parser.parse_args()
+args.old_schedule.close()
+
+##########################################################################################################
+##########################################################################################################
+##########################################################################################################
+
 from datetime import datetime, timedelta
-import pandas as pd
-from openpyxl import Workbook
-from openpyxl.styles import Font, Alignment
-from openpyxl.styles import NamedStyle
 
 YEAR = 2024
+SEMESTER = "spring"
 
 FIST_DAY_OF_CLASSES = datetime(YEAR, 1, 22)
 LAST_DAY_OF_CLASSES = datetime(YEAR, 5, 11)
@@ -15,86 +23,115 @@ LIST_OF_HOLIDAYS = [
 ]
 
 EXAMS = {
-    datetime(YEAR, 3, 20) : "Midterm 1",
+    datetime(YEAR, 3, 6) : "Midterm 1",
     datetime(YEAR, 4, 17) : "Midterm 2",
     datetime(YEAR, 5,  8) : "Midterm 3",
 }
 
-LIST_OF_TOPICS = [
-    "Presentation",
-    "ASP Introduction 11",
-    "ASP introduction cont(choices)",
-    "(project example)",
-    "",
-    "Positive Programs",
-    "Modeling 10",
-    "Modeling",
-    "Modeling/Python",
-    "Python/PropLogic 8",
-    "PropLogic 8/ Normal 1",
-    "Normal 7",
-    "Language(Choice)",
-    "Language(Cardinality Upper)",
-    "Language(Terms&Literals)",
-    "Language",
-    "Modeling II(arithmetic prog)",
-    "Modeling II(Connectivity, yes/no question)",
-    "Modeling II(bounded reachability)",    
-    "Modeling II",    
-    "Ground image",
-    "Ground image",
-]
 
-EXCEL_FILE_PATH = f"schedule_pl_{YEAR}.xlsx"
-# print()
+##########################################################################################################
+##########################################################################################################
+##########################################################################################################
+
+import re
+from pathlib import Path
+from pprint import pprint
+
+import pandas as pd
+from openpyxl import Workbook, load_workbook
+from openpyxl.styles import Font, Alignment
+from openpyxl.styles import NamedStyle
 
 list_of_holidays = [a if isinstance(a, tuple) else (a,a) for a in LIST_OF_HOLIDAYS]
 holy_days = set(start + timedelta(days=day) for start, end in LIST_OF_HOLIDAYS for day in range((end - start).days+1))
-# print(sorted(holy_days))
-# holy_days = { FIST_DAY_OF_CLASSES + day for day in (LAST_DAY_OF_CLASSES - FIST_DAY_OF_CLASSES).days}
 
-def _get_topic(date, topic_index, second_date=None):
-    if day1 in holy_days:
+def new_schedule_name(old_schedule_name):
+    year = re.findall(r"\d{4}", old_schedule_name)
+    if len(year) == 0:
+        year = re.findall(r"\d{2}", old_schedule_name)
+    if len(year) >= 1:
+        return old_schedule_name.replace(year[0], str(YEAR)).replace("spring", SEMESTER).replace("fall", SEMESTER)
+    else:
+        return old_schedule_name.replace("spring", SEMESTER).replace("fall", SEMESTER) + f"_{YEAR}"
+
+old_schedule_file = Path(args.old_schedule.name)
+new_schedule_file = old_schedule_file.parent.joinpath(new_schedule_name(old_schedule_file.stem)+old_schedule_file.suffix)
+
+# print(f"Old schedule: {old_schedule_file}")
+# print(f"New schedule: {new_schedule_file}")
+
+def read_old_schedule(excel_file_path):
+    topics = []
+    workbook = load_workbook(excel_file_path)
+    sheet = workbook[workbook.sheetnames[0]]
+    topic_cells = []
+    topics = []
+    assigments = []
+    for row in sheet.iter_rows(min_row=1, max_row=sheet.max_row):
+        topic_cells.append(row[2])
+        topic_cells.append(row[5])
+        if row[0].value is not None:
+            assigments.append((row[6].value, row[7].value, row[8].value))
+
+    for cell in topic_cells:
+        content = cell.value
+        fill_color = cell.font.color.rgb
+        if content is not None and not content.strip().lower().startswith("recap") and fill_color != "FFFF0000":
+            topics.append(content)
+
+    return topics, assigments
+
+
+def _get_topic(list_of_topics, date, topic_index, second_date=None):
+    if date in holy_days:
         topic = "Holiday"
     elif date in EXAMS:
         topic = EXAMS[date]
     elif second_date is not None and second_date in EXAMS:
         topic = "Recap"
-    elif topic_index < len(LIST_OF_TOPICS):
-        topic = LIST_OF_TOPICS[topic_index]
+    elif topic_index < len(list_of_topics):
+        topic = list_of_topics[topic_index]
         topic_index += 1
     else:
         topic = ""
     return topic, topic_index
 
+
+topics, assigments = read_old_schedule(old_schedule_file)
+
 date = FIST_DAY_OF_CLASSES
 topic_index = 0
+assigments_index = 0
 data = []
 while date <= LAST_DAY_OF_CLASSES:
     day1  = date + timedelta(WEEK_DAY_OF_CLASSES)
     day2 = date + timedelta(WEEK_DAY_OF_CLASSES+2)
-    topic1, topic_index = _get_topic(day1, topic_index, day2)
-    topic2, topic_index = _get_topic(day2, topic_index)
-    row = [date, day1, topic1, "", day2, topic2]
+    topic1, topic_index = _get_topic(topics, day1, topic_index, day2)
+    topic2, topic_index = _get_topic(topics, day2, topic_index)
+    row = [date, day1, topic1, "", day2, topic2] + list(assigments[assigments_index])
     data.append(row)
     date += timedelta(days=7)
+    assigments_index += 1
 
-data = pd.DataFrame(data, columns=["Week", "Date1", "Topic1", "", "Date2", "Topic2"])
+data = pd.DataFrame(data, columns=["Week", "Date1", "Topic1", "", "Date2", "Topic2", "Projects", "HW", "Readings"])
 # data.to_excel("schedule_pl_2024.xlsx", index=False)
 
-with pd.ExcelWriter(EXCEL_FILE_PATH, engine='openpyxl') as writer:
+with pd.ExcelWriter(new_schedule_file, engine='openpyxl') as writer:
     data.to_excel(writer, sheet_name='Sheet1', index=False)
 
     # Get the workbook and the sheet
     workbook = writer.book
     sheet = writer.sheets['Sheet1']
 
-    sheet.column_dimensions['A'].width = 20  # Width of column 'A'
-    sheet.column_dimensions['B'].width = 20  # Width of column 'B'
+    sheet.column_dimensions['A'].width = 15  # Width of column 'A'
+    sheet.column_dimensions['B'].width = 15  # Width of column 'B'
     sheet.column_dimensions['C'].width = 50  # Width of column 'C'
     sheet.column_dimensions['D'].width = 10  # Width of column 'D'
-    sheet.column_dimensions['E'].width = 20  # Width of column 'E'
+    sheet.column_dimensions['E'].width = 15  # Width of column 'E'
     sheet.column_dimensions['F'].width = 50  # Width of column 'F'
+    sheet.column_dimensions['G'].width = 15  # Width of column 'G'
+    sheet.column_dimensions['H'].width = 10  # Width of column 'H'
+    sheet.column_dimensions['I'].width = 10  # Width of column 'I'
 
     # Define date formatting style
     date_format = NamedStyle(name='date_format', number_format='MM/DD/YYYY')
